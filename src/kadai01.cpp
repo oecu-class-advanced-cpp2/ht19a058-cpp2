fe41fc759c22a1123f772348df3c0ca1b36c7e18
gatoanabelu0083 <72872116+gatoanabelu0083@users.noreply.github.com>
2021-11-02 12:36:56 +0900

    kadai1

---
+++ src/kadai01.cpp
@@ -0,0 +1,39 @@
+// kadai1.cpp
+#include <iostream>
+using namespace std;
+
+int nth_prime(int a,int b,int n) {
+	int flag = 0;
+	int count=0;
+	int e = a;
+	while(e<100000){
+		if (e == 1) {
+			flag = 1;
+		}
+		for (int i = 2; i < e; i++) {
+			if (e%i == 0) {
+				flag = 1;
+			}
+		}
+		if (flag == 0) {
+			//printf("%dは素数です\n", e);
+			count++;
+		}
+		if (count == n) {
+			break;
+			cout << 0;
+		}
+		flag = 0;
+		e += b;
+	}
+	return e;
+}
+
+int main()
+{
+	cout << nth_prime(271,37,39) << endl;
+
+	cin.get();
+    cin.get();
+    return 0;
+}
