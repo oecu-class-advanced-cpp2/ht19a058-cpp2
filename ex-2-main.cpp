d9555f7a0f8426d036b305137dd5f38528e1a972
gatoanabelu0083 <72872116+gatoanabelu0083@users.noreply.github.com>
2022-01-23 14:15:57 +0900

    完成

--- ex-2-main.cpp
+++ ex-2-main.cpp
@@ -30,22 +30,45 @@ namespace cpp2 {
 		*/
 		/* ----------------------------------------------------------------- */
 		mcxi(const std::string& s) : value_(0) {
-			int num = 0;
+			int num =0;
 			int sum = 0;
+			int u = 1;
+			int m = 0; int c = 0; int x = 0; int i = 0;
+			int mt = s.find('m'); int ct = s.find('c'); int xt = s.find('x'); int it = s.find('i');
+			string p[4] = { "m","c","x","i" };
+			for (char i:s) {
+				if (i=='m') {
+					m++;
+				}
+				else if (i == 'c') {
+					c++;
+				}
+				else if (i == 'x') {
+					x++;
+				}
+				else if (i == 'i') {
+					i++;
+				}
+			}
+
 			for (auto pos = s.begin(); pos != s.end(); ++pos) {
+				
+				if (*pos == '1' || *pos == '0' ||m>1 || c>1 || x>1 || i>1) {
+					break;
+				}
 				if (*pos >= '2' && *pos <= '9') {
 					num = *pos - '0';
 
 				}
 				else
 				{
-					int u = unit(*pos);
+					
+					u = unit(*pos);
 					value_ += std::max(num, 1)*u;
-					//num = 0;
+					num = 0;
 					//sum += num*u;
 				}
 			}
-			std::cout << value_ << std::endl;
 		}
 
 
@@ -70,7 +93,24 @@ namespace cpp2 {
 		*/
 		/* ----------------------------------------------------------------- */
 		std::string to_string() const {
-			return "XXX";
+			std::string mcxi;
+			int num1 = value_;
+			string s[4] = { "m","c","x","i" };
+			int kotei = 1000;
+			for (int i = 0; i < 4; i++) {
+				if (num1 / kotei >= 1) {
+					if (num1 / kotei == 1) {
+						mcxi += s[i];
+						num1 = num1 - kotei;
+					}
+					else {
+						mcxi += std::to_string(num1 / kotei) + s[i];
+						num1 = num1 - ((num1 / kotei) * kotei);
+					}
+				};
+				kotei = kotei / 10;
+			}
+			return mcxi;
 		}
 
 		void debug_mcxi() {
@@ -93,59 +133,70 @@ namespace cpp2 {
 	};
 }
 
+bool hikaku(string num) {
+	string list[9] = { "3x","x","6cx","5m9c9x9i","m","9m9c9x9i","mi","mx","9m9c9x9i" };
+	for (int i = 0; i < 9; i++) {
+		if (num == list[i]) {
+			return true;
+			break;
+		}
+	}
+	return false;
+}
+
 int main() {
+
 	cpp2::mcxi a0("xi");
 	cpp2::mcxi b0("x9i");
 	auto result0 = a0 + b0;
-	std::cout << "3x" << " " << result0.to_string() << std::endl;
-	
-	cin.get();
-	cin.get();
-	/*
+	cout << hikaku(result0.to_string()) << endl;
+
+
 	cpp2::mcxi a1("i");
 	cpp2::mcxi b1("9i");
 	auto result1 = a1 + b1;
-	std::cout << "x" << " " << result1.to_string() << std::endl;
+	cout << hikaku(result1.to_string()) << endl;
 
 	cpp2::mcxi a2("c2x2i");
 	cpp2::mcxi b2("4c8x8i");
 	auto result2 = a2 + b2;
-	std::cout << "6cx" << " " << result2.to_string() << std::endl;
+	cout << hikaku(result2.to_string()) << endl;
 
 	cpp2::mcxi a3("m2ci");
 	cpp2::mcxi b3("4m7c9x8i");
 	auto result3 = a3 + b3;
-	std::cout << "5m9c9x9i" << " " << result3.to_string() << std::endl;
+	cout << hikaku(result3.to_string()) << endl;
 
 	cpp2::mcxi a4("9c9x9i");
 	cpp2::mcxi b4("i");
 	auto result4 = a4 + b4;
-	std::cout << "m" << " " << result4.to_string() << std::endl;
+	cout << hikaku(result4.to_string()) << endl;
 
 	cpp2::mcxi a5("i");
 	cpp2::mcxi b5("9m9c9x8i");
 	auto result5 = a5 + b5;
-	std::cout << "9m9c9x9i" << " " << result5.to_string() << std::endl;
+	cout << hikaku(result5.to_string()) << endl;
 
 	cpp2::mcxi a6("m");
 	cpp2::mcxi b6("i");
 	auto result6 = a6 + b6;
-	std::cout << "mi" << " " << result6.to_string() << std::endl;
+	cout << hikaku(result6.to_string()) << endl;
 
 	cpp2::mcxi a7("i");
 	cpp2::mcxi b7("m");
 	auto result7 = a7 + b7;
-	std::cout << "mi" << " " << result7.to_string() << std::endl;
+	cout << hikaku(result7.to_string()) << endl;
 
 	cpp2::mcxi a8("m9i");
 	cpp2::mcxi b8("i");
 	auto result8 = a8 + b8;
-	std::cout << "mx" << " " << result8.to_string() << std::endl;
+	cout << hikaku(result8.to_string()) << endl;
 
 	cpp2::mcxi a9("9m8c7xi");
 	cpp2::mcxi b9("c2x8i");
 	auto result9 = a9 + b9;
-	std::cout << "9m9c9x9i" << " " << result9.to_string() << std::endl;
-	*/
+	cout << hikaku(result9.to_string()) << endl;
 
+	cin.get();
+	cin.get();
 }
